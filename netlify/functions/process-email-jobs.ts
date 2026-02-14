77b7da85567488b59f80bfbc4c962573d4bad78b
mahadiul-hasan <mdmahadiulhasan@gmail.com>
2026-02-14 23:32:56 +0600

    fix edge error

--- netlify/functions/process-email-jobs.ts
+++ netlify/functions/process-email-jobs.ts
@@ -1,7 +1,8 @@
-import { processEmailJobs } from "@/lib/email-worker";
+import { processEmailJobs } from "../../lib/email-worker";
 
 export const handler = async () => {
   const result = await processEmailJobs(10);
+
   return {
     statusCode: 200,
     body: JSON.stringify(result),
